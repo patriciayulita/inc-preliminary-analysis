8e6e8cf426f2b403d422b3f4e9b6d523899e0a4a
pgunawan <patricia.gunawan@paediatrics.ox.ac.uk>
2025-09-24 13:43:34 +0100

    tidy up the matlab code

--- code/matlab/IPD_MA_analysis.m
+++ code/matlab/IPD_MA_analysis.m
@@ -26,238 +26,147 @@ end
 
 dataframe_ox = readtable(strcat(path_to_input_folder, '/', dataframe_file_oxford));
 dataframe_ex = readtable(strcat(path_to_input_folder, '/', dataframe_file_exeter));
-dataframe_ucl = readtable(strcat(path_to_input_folder, '/', dataframe_file_ucl));
+
+% Build the file path
+filename = fullfile(path_to_input_folder, dataframe_file_ucl);
+
+% Detect import options
+opts = detectImportOptions(filename);
+
+% Force column 'File' to be string
+opts = setvartype(opts, 'File', 'string');
+dataframe_ucl = readtable(strcat(path_to_input_folder, '/', dataframe_file_ucl),opts);
 
 %% Oxford- validity
 
 % extract all control heel lance data only
 % ----------------------------------------
-grouptable_cl = grouptable(strcmp(grouptable.Event,'cl'),:);
+dataframe_ox_cl = dataframe_ox(strcmp(dataframe_ox.Event,'cl'),:);
 
 % save control lance data (single event per participant)
-table_name_csv = 'grouptable_controllance.csv';
-writetable(grouptable_cl,fullfile(path_to_output_folder,table_name_csv));
+table_name_csv = 'dataframe_ox_controllance.csv';
+writetable(dataframe_ox_cl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all heel lance data only
 % --------------------------------
-grouptable_hl = grouptable(strcmp(grouptable.Event,'hl'),:);
+dataframe_ox_hl = dataframe_ox(strcmp(dataframe_ox.Event,'hl'),:);
 
 % save heel lance data (single event per participant)
-table_name_csv = 'grouptable_heellance.csv';
-writetable(grouptable_hl,fullfile(path_to_output_folder,table_name_csv));
+table_name_csv = 'dataframe_ox_heellance.csv';
+writetable(dataframe_ox_hl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all background data only
 % --------------------------------
-grouptable_bg = grouptable(strcmp(grouptable.Event,'bg'),:);
+dataframe_ox_bg = dataframe_ox(strcmp(dataframe_ox.Event,'bg'),:);
 
 % save background data (single event per participant)
-table_name_csv = 'grouptable_bg.csv';
-writetable(grouptable_bg,fullfile(path_to_output_folder,table_name_csv));
+table_name_csv = 'dataframe_ox_bg.csv';
+writetable(dataframe_ox_bg,fullfile(path_to_output_folder,table_name_csv));
 
 %% Oxford- interpretability
 
 % extract all noxious
 % ----------------------------------------
-grouptable_nox = grouptable(strcmp(grouptable.Event,'hl') | strcmp(grouptable.Event, 'imm') ...
-    | strcmp(grouptable.Event, 'lp') | strcmp(grouptable.Event, 'cn'),:);
+dataframe_ox_nox = dataframe_ox(strcmp(dataframe_ox.Event,'hl') | strcmp(dataframe_ox.Event, 'imm') ...
+    | strcmp(dataframe_ox.Event, 'lp') | strcmp(dataframe_ox.Event, 'cn'),:);
 
 % save noxious data
-table_name_csv = 'grouptable_nox.csv';
-table_name = 'grouptable_nox';
-destinationFolder1 = "/Volumes/Extreme SSD/IPD-MA project/result/Oxford_data_analysis/";
-
-writetable(grouptable_nox,fullfile(destinationFolder1,table_name_csv));
-save(fullfile(destinationFolder1,table_name),'grouptable_nox');
-
-destinationFolder2 = "/Users/patriciagunawan/Library/CloudStorage/OneDrive-Nexus365/Oxford S3/IPD-MA project/Validity/Oxford_data_analysis/";
-
-writetable(grouptable_nox,fullfile(destinationFolder2,table_name_csv));
+table_name_csv = 'dataframe_ox_nox.csv';
+writetable(dataframe_ox_nox,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all innocuous
 % ----------------------------------------
-grouptable_innoc = grouptable(strcmp(grouptable.Event,'cl') | strcmp(grouptable.Event, 'tf'),:);
+dataframe_ox_innoc = dataframe_ox(strcmp(dataframe_ox.Event,'cl') | strcmp(dataframe_ox.Event, 'tf'),:);
 
 % save innocuous data
-table_name_csv = 'grouptable_innoc.csv';
-table_name = 'grouptable_innoc';
-destinationFolder1 = "/Volumes/Extreme SSD/IPD-MA project/result/Oxford_data_analysis/";
-
-writetable(grouptable_innoc,fullfile(destinationFolder1,table_name_csv));
-save(fullfile(destinationFolder1,table_name),'grouptable_innoc');
-
-destinationFolder2 = "/Users/patriciagunawan/Library/CloudStorage/OneDrive-Nexus365/Oxford S3/IPD-MA project/Validity/Oxford_data_analysis/";
-
-writetable(grouptable_innoc,fullfile(destinationFolder2,table_name_csv));
-
-%% Exeter
-
-destinationFolder = '/Volumes/Extreme SSD/IPD-MA project/result/'; % Patricia OS Mac (lab PC)
-
-% Load EEG
-ALLEEG = loadToALLEEGStruct(id_ex, destinationFolder, 'all');
-
-% Stack data, NRF and QC features
-NRFtable = table_from_NRF(ALLEEG, 'noxious_NRF_500Hz'); % table of NRF values
-datatable = table_from_data(ALLEEG,[]); % table with raw data
-% table_qc = table_from_qc(ALLEEG, {'CZ'}); % table with qc features
-% [~, table_qc] = InteractiveQCRaincloud(table_qc, 'zscored'); % get z-scores for qc features
-
-% Merge individual tables
-grouptable = join(NRFtable, datatable);
-% grouptable = join(grouptable, table_qc);
-
-% Note: if you wanted to remove epochs with qc feature 1 |z| > 3,
-% you would run:
-% grouptable(grouptable.zscores(:,1) > 3, :) = []; 
-
-grouptable_ex = grouptable(:,1:5);
-
-% save grouptable_save
-table_name_csv = 'dataframe_exeter.csv';
-table_name = 'dataframe_exeter';
-
-writetable(grouptable_ex,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_ex');
+table_name_csv = 'dataframe_ox_innoc.csv';
+writetable(dataframe_ox_innoc,fullfile(path_to_output_folder,table_name_csv));
 
 %% Exeter - validity
  
 % extract all control heel lance data only
 % ----------------------------------------
-grouptable_cl = grouptable(strcmp(grouptable.Event,'cl'),:);
+dataframe_ex_cl = dataframe_ex(strcmp(dataframe_ex.Event,'cl'),:);
 
 % save control lance data (single event per participant)
-table_name_csv = 'grouptable_controllance.csv';
-table_name = 'grouptable_controllance';
-destinationFolder = "/Volumes/Extreme SSD/IPD-MA project/result/Exeter_data_analysis/";
-
-writetable(grouptable_cl,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_cl');
+table_name_csv = 'dataframe_ex_controllance.csv';
+writetable(dataframe_ex_cl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all heel lance data only
 % --------------------------------
-grouptable_hl = grouptable(strcmp(grouptable.Event,'hl'),:);
+dataframe_ex_hl = dataframe_ex(strcmp(dataframe_ex.Event,'hl'),:);
 
 % save heel lance data (single event per participant)
-table_name_csv = 'grouptable_heellance.csv';
-table_name = 'grouptable_heellance';
-
-writetable(grouptable_hl,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_hl');
+table_name_csv = 'dataframe_ex_heellance.csv';
+writetable(dataframe_ex_hl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all background data only
 % --------------------------------
-grouptable_bg = grouptable(strcmp(grouptable.Event,'bg'),:);
+dataframe_ex_bg = dataframe_ex(strcmp(dataframe_ex.Event,'bg'),:);
 
 % save background data (single event per participant)
-table_name_csv = 'grouptable_bg.csv';
-table_name = 'grouptable_bg';
-
-writetable(grouptable_bg,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_bg');
+table_name_csv = 'dataframe_ex_bg.csv';
+writetable(dataframe_ex_bg,fullfile(path_to_output_folder,table_name_csv));
 
 %% Exeter - interpretability
 
 % extract all noxious
 % ----------------------------------------
-% Exeter's nox is only 'hl', so grouptable_nox = grouptable_heellance
+% Exeter's nox is only 'hl', so dataframe_ex_nox = dataframe_ex_heellance
 
 % extract all innocuous
 % ----------------------------------------
-grouptable_innoc = grouptable(strcmp(grouptable.Event,'cl') ...
-    | strcmp(grouptable.Event, 'tf'),:);
+dataframe_ex_innoc = dataframe_ex(strcmp(dataframe_ex.Event,'cl') ...
+    | strcmp(dataframe_ex.Event, 'tf'),:);
 
 % save innocuous data
-table_name_csv = 'grouptable_innoc.csv';
-table_name = 'grouptable_innoc';
-destinationFolder = "/Volumes/Extreme SSD/IPD-MA project/result/Exeter_data_analysis/";
-
-writetable(grouptable_innoc,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_innoc');
-
-%% UCL
-
-destinationFolder = '/Volumes/Extreme SSD/IPD-MA project/result/UCL_data'; % Patricia OS Mac (lab PC)
-
-% Load EEG
-ALLEEG = loadToALLEEGStruct(id_ucl, destinationFolder, 'all','*');
-
-% Stack data, NRF and QC features
-NRFtable = table_from_NRF(ALLEEG, 'noxious_NRF_500Hz'); % table of NRF values
-datatable = table_from_data(ALLEEG,[]); % table with raw data
-% table_qc = table_from_qc(ALLEEG, {'CZ'}); % table with qc features
-% [~, table_qc] = InteractiveQCRaincloud(table_qc, 'zscored'); % get z-scores for qc features
-
-% Merge individual tables
-grouptable = join(NRFtable, datatable);
-% grouptable = join(grouptable, table_qc);
-
-% Note: if you wanted to remove epochs with qc feature 1 |z| > 3,
-% you would run:
-% grouptable(grouptable.zscores(:,1) > 3, :) = []; 
-
-grouptable_ucl = grouptable(:,1:5);
-
-% save grouptable_save
-table_name_csv = 'dataframe_ucl.csv';
-table_name = 'dataframe_ucl';
-
-writetable(grouptable_ucl,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_ucl');
+table_name_csv = 'dataframe_ex_innoc.csv';
+writetable(dataframe_ex_innoc,fullfile(path_to_output_folder,table_name_csv));
 
 %% UCL - validity
  
 % extract all control heel lance data only
 % ----------------------------------------
-grouptable_cl = grouptable(strcmp(grouptable.Event,'cl'),:);
-grouptable_cl.setname = grouptable_cl.File;
+dataframe_ucl_cl = dataframe_ucl(strcmp(dataframe_ucl.Event,'cl'),:);
+dataframe_ucl_cl.setname = dataframe_ucl_cl.File;
 
 % save control lance data (single event per participant)
-table_name_csv = 'grouptable_controllance.csv';
-table_name = 'grouptable_controllance';
-destinationFolder = "/Volumes/Extreme SSD/IPD-MA project/result/UCL_data_analysis/";
-
-writetable(grouptable_cl,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_cl');
+table_name_csv = 'dataframe_ucl_controllance.csv';
+writetable(dataframe_ucl_cl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all heel lance data only
 % --------------------------------
-grouptable_hl = grouptable(strcmp(grouptable.Event,'hl'),:);
+dataframe_ucl_hl = dataframe_ucl(strcmp(dataframe_ucl.Event,'hl'),:);
 
 % Remove "_2" only if it appears at the end of a name
-grouptable_hl.setname = regexprep(grouptable_hl.File, '_2$', '');
+dataframe_ucl_hl.setname = regexprep(dataframe_ucl_hl.File, '_2$', '');
 
 % save heel lance data (single event per participant)
-table_name_csv = 'grouptable_heellance.csv';
-table_name = 'grouptable_heellance';
-
-writetable(grouptable_hl,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_hl');
+table_name_csv = 'dataframe_ucl_heellance.csv';
+writetable(dataframe_ucl_hl,fullfile(path_to_output_folder,table_name_csv));
 
 % extract all background data only
 % --------------------------------
-grouptable_bg = grouptable(strcmp(grouptable.Event,'bg'),:);
+dataframe_ucl_bg = dataframe_ucl(strcmp(dataframe_ucl.Event,'bg'),:);
 
 % Remove "_2" only if it appears at the end of a name
-grouptable_bg.setname = regexprep(grouptable_bg.File, '_2$', '');
+dataframe_ucl_bg.setname = regexprep(dataframe_ucl_bg.File, '_2$', '');
 
-[uniqueID, ibg] = unique(grouptable_bg.setname,'first');
-grouptable_bg = grouptable_bg(ibg,:);
+[uniqueID, ibg] = unique(dataframe_ucl_bg.setname,'first');
+dataframe_ucl_bg = dataframe_ucl_bg(ibg,:);
 
 % save background data (single event per participant)
-table_name_csv = 'grouptable_bg.csv';
-table_name = 'grouptable_bg';
-
-writetable(grouptable_bg,fullfile(destinationFolder,table_name_csv));
-save(fullfile(destinationFolder,table_name),'grouptable_bg');
+table_name_csv = 'dataframe_ucl_bg.csv';
+writetable(dataframe_ucl_bg,fullfile(path_to_output_folder,table_name_csv));
 
 %% UCL - interpretability
 % extract all noxious
 % ----------------------------------------
-% UCL's nox is only 'hl', so grouptable_nox = grouptable_heellance
+% UCL's nox is only 'hl', so dataframe_ucl_nox = dataframe_ucl_heellance
 
 % extract all innocuous
 % ----------------------------------------
-% UCL's innoc is only 'cl', so grouptable_innoc = grouptable_controllance
+% UCL's innoc is only 'cl', so dataframe_ucl_innoc = dataframe_ucl_controllance
 
 %% validity: between-sites averaging
 
@@ -303,34 +212,3 @@ save(fullfile(destinationFolder,table_name),'grouptable_bg');
 
 % run interpretability_2.R (the code underneath the typical magnitude
 % calculation)
-
-%% this is the default example from SIMPLE pipeline, haven't customised yet
-% Plot raw & jitter-corrected EEG average
-% ----------------------------------------
-% Using the table generated above, we can now plot the average 
-% EEG traces to visually compare responses to the different events. 
-
-time = ALLEEG(1).xmin:1/ALLEEG(1).srate:ALLEEG(1).xmax; time = time * 1000;
-NRF_start = 400; NRFName = 'noxious_NRF_500Hz';
-events = {'dummy','bg','cl','hl'};
-
-plot_average_NRF_responses(grouptable, events, time, NRFName, NRF_start)
-
-% plot control vs noxious NRF magnitudes
-% ---------------------------------------
-index_hl = ismember([datatable.Event], {'hl'});
-index_cl = ismember([datatable.Event], {'cl'});
-
-% to plot paired data, the samples must be the same size. You will get an
-% error otherwise.
-
-figure
-gardnerAltmanPlot(NRFtable.Coef(index_cl), NRFtable.Coef(index_hl),'Paired',true)
-set(gca,'XTickLabel',{'Control heel lance','Heel lance', 'Mean difference'})
-ylabel('NRF magnitude')
-
-
-
-
-
-
